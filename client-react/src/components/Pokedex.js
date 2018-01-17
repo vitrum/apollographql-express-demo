3853a6a39239f2ced0a20270321a4fe20f7c72b4
vitrum.zhu <vitrum.zhu@99bill.com>
2018-01-17 11:40:01 +0800

    添加文件，调整依赖

---
+++ client-react/src/components/Pokedex.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.div`
+  color: #7F7F7F;
+  font-size: 32px;
+  font-weight: 300;
+`
+
+export default class Pokedex extends React.Component {
+
+  render () {
+    return (
+      <div className='w-100 bg-light-gray min-vh-100'>
+        <Title className='tc pa5'>
+          Hey, there are 0 Pokemons in your pokedex...
+        </Title>
+      </div>
+    )
+  }
+}
